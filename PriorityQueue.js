432b28cfb8fb88c4921f74267ef94e9b20857134
kevinmoore444 <kevinmoore444@gmail.com>
2023-04-05 06:59:15 -0700

    commit

---
+++ PriorityQueue.js
@@ -0,0 +1,95 @@
+class PriorityQueue {
+    constructor(){
+        this.values = [];
+    }
+
+
+    enqueue(val, priority){
+        let newNode = new Node(val, priority);
+        this.values.push(newNode)
+        this.bubbleUp();
+    }
+
+    //Helper function for insert
+    bubbleUp(element){
+        //This is the newly inserted element
+        var index = this.length-1
+        
+        while(true){
+            let parentIndex = Math.floor(index-1)/2;
+            let parent = this.values[parentIndex]
+                if(element.priority >= parent.priority){
+                    break;
+                }
+                this.values[parentIndex] = element;
+                this.values[index] = parent;
+                index = parentIndex;
+            }
+
+    }
+
+    dequeue(){
+        //Pop off the last value and insert it at the head (max). 
+        //Then perform sinkdown to get that new head into it's proper place.
+        const min = this.values[0]
+        const end = this.values.pop();
+        this.values[0] = end;
+        //Condition if there is only one value left in the binary heap. You will pop it and leave nothing left, 
+        //(as opposed to reinserting that value at the head and sinking down)
+        if(this.values.length > 0) {
+        this.sinkDown();
+        }
+        return min;
+    }
+
+    //Helper Function for extractMax
+    sinkDown(){
+        let idx = 0;
+        const length = this.values.length;
+        //This is the element you swapped in during extractMax which must sink down
+        const element = this.values[0];
+
+        while(true){
+            let leftChildIdx = 2 * idx +1        
+            let rightChildIdx = 2 * idx + 2
+            let leftChild, rightChild;
+            let swapFlag = null;
+
+            //If the value of the sinking down element is greater than the value of it's left child, then flag that
+            //as the one to be swapped later down in the code
+            if(leftChildIdx < length){
+                leftChild = this.values[leftChildIdx];
+                if(leftChild.priority < element.priority){
+                    swapFlag = leftChildIdx;
+                }
+            }
+
+            //Same logic as the block of code above, BUT, if the right child is greater than the left child,
+            //then the parent needs to swap with the right child and the swap flag is overwritten. 
+            if(rightChildIdx < length){
+                rightChild = this.values[rightChildIdx]
+                if((swapFlag === null && rightChild.priority < element.priority) || (swapFlag !== null && rightChild.priority < leftChild.priority)) 
+                    {
+                        swapFlag = rightChildIdx;
+                    }
+            }
+            //If you haven't swapped in the above loop, then break out of the loop and return to extractMax function
+            if(swapFlag === null) break;
+            //If you have identified a swap between parent and child, then perform the swap:
+            this.values[idx] = this.values[swapFlag]
+            this.values[swapFlag] = element
+            idx = swapFlag;
+        }
+
+    }
+}
+    class Node {
+        constructor(val, priority){
+            this.val = val;
+            this.priority = priority;
+        }
+    }
+
+
+
+
